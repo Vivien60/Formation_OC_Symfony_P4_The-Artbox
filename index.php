777004b022f67d35213334bbca787f5699c23fda
Vivien60 <phedras.ch@gmail.com>
2025-07-17 18:24:25 +0200

    Application de DRY pour le header et le footer des pages

--- index.php
+++ index.php
@@ -5,12 +5,9 @@ require_once "include/oeuvres.php";
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>The ArtBox</title>
+<?php
+require_once "include/headerHTML.php";
+?>
 </head>
 <body>
 <?php
