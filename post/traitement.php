ecee5d192433a32729837ea27e9277965ac5ce02
Vivien60 <phedras.ch@gmail.com>
2025-08-06 19:13:49 +0200

    fix sécurtité et gestion d'erreurs

--- post/traitement.php
+++ post/traitement.php
@@ -6,11 +6,11 @@ require_once "../include/oeuvre.php";
 require_once "../view/components/formPostLib.php";
 
 ob_start();
-$title = htmlentities($_POST['title']);
-$artist = htmlentities($_POST['artist']);
-$desc = htmlentities($_POST['description']);
-$imagUrl = filter_input(INPUT_POST, 'image_link', FILTER_SANITIZE_URL);;
-
+$title = htmlspecialchars($_POST['title']);
+$artist = htmlspecialchars($_POST['artist']);
+$desc = htmlspecialchars($_POST['description']);
+$imagUrl = htmlspecialchars(filter_input(INPUT_POST, 'image_link', FILTER_SANITIZE_URL));
+var_dump($imagUrl);
 $hasError = false;
 if(empty($title)) {
     $hasError = true;
@@ -30,6 +30,7 @@ if(empty($artist)) {
 }
 if(!empty($imagUrl)) {
     $urlScheme = parse_url($imagUrl, PHP_URL_SCHEME);
+    var_dump($urlScheme);
     if(!($urlScheme === 'http' || $urlScheme === 'https')) {
         $hasError = true;
         displayError("L'URL de l'image est incorrecte");
